3217139dfcedfa3ce3aeb6e0be027f018db9c3d0
Matthew Rao <mat_rao@hotmail.com>
2023-06-26 14:52:00 -0400

    Project card is finished!

--- client/src/components/Projects/Web/WebCard/WebCard.js
+++ client/src/components/Projects/Web/WebCard/WebCard.js
@@ -1,88 +1,71 @@
 import React from 'react'
 import './WebCard.css'
-import pic1 from '../../../../Content/pic01.jpg';
-import pic2 from '../../../../Content/pic02.jpg';
-import pic3 from '../../../../Content/pic03.jpg';
 
-function WebCard() {
+function WebCard({ title, features, desc, imageUrls, webUrls }) {
     return (
         <>
-            <div className='container-fluid p-4'>
-                <div className='background'>
-                    <div className='row mt-4'>
-                        <h4><strong>Carte Blanche</strong></h4>
-                    </div>
-                    <div className='gallery'>
-                        <article className='webCard'>
-                            <figure>
-                                <img
-                                    src={pic1}
-                                    alt='preview'
-                                    title='preview'
-                                />
-
-                            </figure>
-                        </article>
-                        <article className='webCard'>
-                            <figure>
-                                <img
-                                    src={pic2}
-                                    alt='preview'
-                                    title='preview'
-                                />
-
-                            </figure>
-                        </article>
-
-                        <article className='webCard'>
-                            <figure>
-                                <img
-                                    src={pic3}
-                                    alt='preview'
-                                    title='preview'
-                                />
-
-                            </figure>
-                        </article>
-                        <article className='webCard'>
-                            <figure>
-                                <img
-                                    src={pic3}
-                                    alt='preview'
-                                    title='preview'
-                                />
-
-                            </figure>
-                        </article>
-                        <article className='webCard'>
-                            <figure>
-                                <img
-                                    src={pic3}
-                                    alt='preview'
-                                    title='preview'
-                                />
-
-                            </figure>
-                        </article>
-                    </div>
-                    <div className='row pt-3 w-75'>
-                        <div className='col'>
-                            <p><strong>Features</strong></p>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item"><strong>Login Portal</strong> for Users and Admin</li>
-                                <li className="list-group-item"><strong>Admin dashboard</strong> for managing users and events</li>
-                                <li className="list-group-item"><strong>REST</strong>ful API</li>
-                            </ul>
+            <div className='row'>
+                <div className='container'>
+                    <div className='background'>
+                        <div className='row mt-4'>
+                            <h4><strong>{title}</strong></h4>
                         </div>
-                        <div className='col w-100'>
-                            <p><strong>Technologies</strong></p>
-                            <p>The Project was developed in a team of 6 using C#. We designed our database with PostGreSQL and used ASP.NetCore to develope a MVC RESTful API. For our front-end we used Blazor WebAssembly Framework. Authentication was done using JWT tokens. We originally deployed the project using Azure. Many other dependencies and technologies were used, I highly recommend checking out the github page for this project.</p>
+                        <div className='gallery'>
+
+                            {imageUrls.map((image, index) => (
+                                <article className='webCard'>
+                                    <figure>
+                                        <img
+                                            key={index}
+                                            src={image}
+                                            alt={`${title} - Image ${index}`}
+                                            title='Preview of Project'
+                                        />
+
+                                    </figure>
+                                </article>
+                            ))}
                         </div>
+                        <div className='row pt-3 w-75'>
+                            <div className='col'>
+                                <p><strong>Features</strong></p>
+                                <ul className="list-group list-group-flush">
+                                    {features.map((feature, index) => (
+                                        <li
+                                            key={index}
+                                            className="list-group-item">{feature}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className='col w-100'>
+                                <p><strong>Technologies</strong></p>
+                                <p>{desc}</p>
+                            </div>
+                        </div>
+                        <div className='row pb-4'>
+
+                            {webUrls.map((url, urlIndex) => (
+                                <div key={urlIndex}>
+                                    {url.map((button, linkIndex) => (
+                                        <a
+                                            key={linkIndex}
+                                            href={button.link}
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                        >
+                                            <button className='visit-button'><strong>{button.text}</strong></button>
+                                        </a>
+                                    ))}
+                                </div>
+                            ))}
+
+                        </div>
+
+
                     </div>
-
-
                 </div>
             </div>
+
         </>
     )
 }
--- client/src/components/Projects/Web/WebProject.js
+++ client/src/components/Projects/Web/WebProject.js
@@ -1,5 +1,82 @@
 import React from 'react'
 import WebCard from './WebCard/WebCard'
+import pic1 from '../../../Content/carteblanche1.jpg';
+import pic2 from '../../../Content/carteblanche2.jpg';
+import pic3 from '../../../Content/carteblanche3.jpg';
+import pic4 from '../../../Content/carteblanche4.jpg';
+import pic5 from '../../../Content/carteblanche.jpg';
+import pic6 from '../../../Content/moonrise.jpg'
+import pic7 from '../../../Content/moonrise1.jpg'
+import pic9 from '../../../Content/moonrise4.jpg'
+import pic10 from '../../../Content/moonrise3.jpg'
+//cart blanche
+
+
+
+const carteBlancheTitle = 'Carte Blanche'
+
+const carteBlanchePics = [
+    pic1,
+    pic2,
+    pic3,
+    pic4,
+    pic5
+]
+const carteBlancheFeatures = [
+    'Login Portal for Users and Admin',
+    'JWT Authentification',
+    'Admin Dashboard',
+    'Create, Read, Update & Delete',
+    'RESTful API'
+]
+const carteBlancheDesc = 'Developed in a team of 6 using C#. Front-end features Blazor WebAssembly Framework and the back-end was designed and developped with ASP.NetCore. We designed our PosteGreSQL database with many controllers in mind.'
+
+const carteBlancheUrls = [
+
+    [
+        {text: 'Visit Website', link: 'https://fsd-04-carteblanche.azurewebsites.net/'}
+    ],
+    [
+        {text: 'View Code', link: 'https://github.com/KylePiche/FSD04-Integration'}
+    ]
+
+]
+
+//moonrise movies
+
+const moonTitle = 'MoonRise Movies'
+
+const moonPics = [
+    pic6,
+    pic7,
+    pic9,
+    pic10
+]
+
+const moonFeat = [
+    'Login Portal for Users and Admin',
+    'Stripe API',
+    'Admin Dashboard',
+    'Create, Read, Update & Delete',
+    'RESTful API'
+]
+
+const moonDesc = 'Developed in a team of 3 using C#. We used Razor Pages for our front-end and ASP.Net for the back-end. Database was designed and implemented using Azure, we went the same route for deploying. Stripe was used for purchasing tickets.'
+
+const moonUrls = [
+
+
+
+    [
+        {text: 'Visit Website', link: 'https://moonrisemovies.azurewebsites.net/'}
+    ],
+    [
+        {text: 'View Code', link: 'https://github.com/mitchellfridman/MoonriseMovies'}
+    ]
+
+]
+
+//react project
 
 function WebProject() {
     return (
@@ -7,10 +84,25 @@ function WebProject() {
             <div className='content'>
                 <div className='container-md p-4'>
                     <div className='title'>
-                    <h1><strong> <small>Stepping Up the Game:<br /></small>Spotlighting my Projects</strong></h1>
+                        <h1><strong> <small>Stepping Up the Game:<br /></small>Spotlighting my Projects</strong></h1>
                     </div>
-                    <div className='container-fluid text-center'>
-                        <WebCard/>
+                    <div className='container text-center'>
+                        <div className='pt-4'>
+                            <WebCard
+                                title={carteBlancheTitle}
+                                features={carteBlancheFeatures}
+                                desc={carteBlancheDesc}
+                                imageUrls={carteBlanchePics}
+                                webUrls={carteBlancheUrls}
+                            />
+                            <WebCard
+                                title={moonTitle}
+                                features={moonFeat}
+                                desc={moonDesc}
+                                imageUrls={moonPics}
+                                webUrls={moonUrls}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
